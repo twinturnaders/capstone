e776e10d62a292b3898cec1ba9d7e552fae3a867
Brina Bright <bsonju1@wgu.edu>
2025-09-02 15:13:21 -0600

    A bunch of stuff completed front-end and back-end sync: Completed & Working (still improving): App-home user-account user-bill add user-bill view compare auths working

---
+++ capstoneFrontEnd/src/app/user/user-account/compare/compare.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {SearchComponent} from '../../../view-compare/search/search.component';
+
+@Component({
+  selector: 'app-compare',
+  imports: [SearchComponent],
+  templateUrl: './compare.component.html',
+  styleUrl: './compare.component.css',
+})
+export class CompareComponent {
+
+}
